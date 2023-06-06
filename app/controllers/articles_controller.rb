0a82d308e0b90a6e87acfab39415ee2aedb48caa
Skye <anastasia_sorokina_90@mail.ru>
2023-06-06 19:09:32 +0300

    error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,18 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
-		
+		@article = Article.new
 	end
 
 	def create
 		@article = Article.new(params.require(:article).permit(:title, :description))
-		@article.save
-		redirect_to article_path(@article.id)
+		if @article.save
+			flash[:notice] = "Article was created successfully"
+			redirect_to article_path(@article.id)
+		else
+			Rails.logger.info @article.errors.full_messages
+			render 'new'
+		end
 	end
 
 end
